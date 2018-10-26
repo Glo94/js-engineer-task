dde7d67c3937dcf4c741ce56d90092f64a194ee0
Gloria Rizzi <gloria.rizzi@nttdata.com>
2018-10-26 17:26:33 +0200

    add car details

--- src/routes/CarDetails/CarDetails.jsx
+++ src/routes/CarDetails/CarDetails.jsx
@@ -1,9 +1,45 @@
 import React from "react";
 
-const CarDetails = () => (
-  <div>
-    <img src="" alt="" />
-  </div>
-);
+class CarDetails extends React.Component {
+  render() {
+    const {
+      pictureUrl,
+      manufacturerName,
+      modelName,
+      fuelType,
+      mileage,
+      stockNumber,
+      color
+    } = this.props.location.state;
+
+    return (
+      <div className="car-details-company">
+        <div className="car-details-company__img">
+          <img src={pictureUrl} alt="" />
+        </div>
+        <div className="car-details-company__left">
+          <div>{manufacturerName + " " + modelName}</div>
+          <div>
+            {`Stock # ${stockNumber} - ${
+              mileage.number
+            } KM - ${fuelType} - ${color}`}
+          </div>
+          <div className="car-details-company__left__text">
+            This car is currently available and can be delivered as soon as
+            tomorrow morning. Please be aware that delivery times shown in this
+            page are not definitive and may change due to bad weather conditions
+          </div>
+        </div>
+        <div className="car-details-company__right">
+          <div className="ar-details-company__right__text">
+            If you like this car, click the button and save it in your
+            collection of favourite items.
+          </div>
+          <button className="ar-details-company__right__btn">Save</button>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default CarDetails;
--- src/routes/Home/components/Car/Car.jsx
+++ src/routes/Home/components/Car/Car.jsx
@@ -1,19 +1,29 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Car = ({ label, car }) => (
-  <div className="car-company">
-    <div className="car-company__img">
-      <img alt={car.modelName} src={car.pictureUrl} />
-    </div>
-    <div className="car-company__details">
-      <label>{`${car.manufacturerName} ${car.modelName}`}</label>
-      <label>{`Stock # ${car.stockNumber} - ${car.mileage.number} KM - ${
-        car.fuelType
-      } - ${car.color}`}</label>
-      <Link to={`/car-details/${car.stockNumber}`}>View details</Link>
-    </div>
-  </div>
-);
+class Car extends React.Component {
+  render() {
+    const { label, car } = this.props;
+
+    return (
+      <div className="car-company">
+        <div className="car-company__img">
+          <img alt={car.modelName} src={car.pictureUrl} />
+        </div>
+        <div className="car-company__details">
+          <label>{`${car.manufacturerName} ${car.modelName}`}</label>
+          <label>{`Stock # ${car.stockNumber} - ${car.mileage.number} KM - ${
+            car.fuelType
+          } - ${car.color}`}</label>
+          <Link
+            to={{ pathname: `/car-details/${car.stockNumber}`, state: car }}
+          >
+            View details
+          </Link>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Car;

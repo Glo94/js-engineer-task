348ff414f65d89058335e45784514d2bb3e25fca
Gloria Rizzi <gloria.rizzi@nttdata.com>
2018-11-03 17:14:44 +0100

    add 404 url

--- src/routes/CarDetails/CarDetails.jsx
+++ src/routes/CarDetails/CarDetails.jsx
@@ -20,8 +20,13 @@ import {
   reciveCarDetails
 } from '../../redux/actions/loadActions'
 
+import NoMatch from '../NoMatch/NoMatch'
+
 
 class CarDetails extends Component {
+  state = {
+    error: false
+  }
 
   handleClick = () => {
     const { dispatch, wishList, car } = this.props;
@@ -51,54 +56,63 @@ class CarDetails extends Component {
       let car = getCarStockNumber("http://localhost:3001/cars/"+cod);
       Promise.resolve(car).then(result => {
         dispatch(reciveCarDetails(result.car));
-      });
+      }).catch(err => 
+        this.setState({
+        error: true
+      }));
     }
   }
 
   render() {
     const {car} = this.props;
     return (
-      <div className="car-details-company">
-        <div className="car-details-company__img">
-          <img src={car.pictureUrl} alt="" />
-        </div>
-        <div className="car-details-company__container">
-          <div className="car-details-company__container__left column column--7">
-            <div className="car-details-company__container__left__title">
-              {car.manufacturerName + " " + car.modelName}
+      <React.Fragment>
+        {
+          this.state.error
+          ? <NoMatch />
+          : <div className="car-details-company">
+            <div className="car-details-company__img">
+              <img src={car.pictureUrl} alt="" />
             </div>
-            <div className="car-details-company__container__left__sub">
-              {`Stock # ${car.stockNumber} - ${
-                car.mileage.number
-              } KM - ${car.fuelType} - ${car.color}`}
-            </div>
-            <div className="car-details-company__container__left__text">
-              This car is currently available and can be delivered as soon as
-              tomorrow morning. Please be aware that delivery times shown in
-              this page are not definitive and may change due to bad weather
-              conditions
-            </div>
-          </div>
-          <div className="car-details-company__container__right  column column--5">
-            <div className="car-details-company__container__right__wishlist">
-              <div className="ar-details-company__container__right__wishlist__text">
-              {this.props.wishList.includes(car.stockNumber)
-                ? "This car is in your collection. Click the button and remove it!"
-                : "If you like this car, click the button and save it in your collection of favourite items."
-              }
+            <div className="car-details-company__container">
+              <div className="car-details-company__container__left column column--7">
+                <div className="car-details-company__container__left__title">
+                  {car.manufacturerName + " " + car.modelName}
+                </div>
+                <div className="car-details-company__container__left__sub">
+                  {`Stock # ${car.stockNumber} - ${
+                    car.mileage.number
+                  } KM - ${car.fuelType} - ${car.color}`}
+                </div>
+                <div className="car-details-company__container__left__text">
+                  This car is currently available and can be delivered as soon as
+                  tomorrow morning. Please be aware that delivery times shown in
+                  this page are not definitive and may change due to bad weather
+                  conditions
+                </div>
               </div>
-              <div className="car-details-company__container__right__wishlist__btn">
-                <button onClick={this.handleClick}>
+              <div className="car-details-company__container__right  column column--5">
+                <div className="car-details-company__container__right__wishlist">
+                  <div className="ar-details-company__container__right__wishlist__text">
                   {this.props.wishList.includes(car.stockNumber)
-                    ? "Remove"
-                    : "Save"}
-                </button>
+                    ? "This car is in your collection. Click the button and remove it!"
+                    : "If you like this car, click the button and save it in your collection of favourite items."
+                  }
+                  </div>
+                  <div className="car-details-company__container__right__wishlist__btn">
+                    <button onClick={this.handleClick}>
+                      {this.props.wishList.includes(car.stockNumber)
+                        ? "Remove"
+                        : "Save"}
+                    </button>
+                  </div>
+                </div>
               </div>
             </div>
+            <NotificationContainer />
           </div>
-        </div>
-        <NotificationContainer />
-      </div>
+        }
+      </React.Fragment>
     );
   }
 }

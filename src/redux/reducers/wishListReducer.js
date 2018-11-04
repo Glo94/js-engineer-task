64b44a57976398cd449ce5528ba790622746c37e
Gloria Rizzi <gloria.rizzi@nttdata.com>
2018-11-04 13:06:32 +0100

    fix

--- src/redux/reducers/wishListReducer.js
+++ src/redux/reducers/wishListReducer.js
@@ -3,11 +3,11 @@ import initialState from "./initialState";
 
 export function wishListReducer(state = initialState.wishList, action) {
   switch (action.type) {
-    case types.ADD_CAR_WISHLIST_SUCCESS:
-      return [...state, action.a];
-    case types.REMOVE_CAR_WISHLIST_SUCCESS:
-      return state.filter(item => item !== action.b);
-    default:
-      return state;
+  case types.ADD_CAR_WISHLIST_SUCCESS:
+    return [...state, action.a];
+  case types.REMOVE_CAR_WISHLIST_SUCCESS:
+    return state.filter(item => item !== action.b);
+  default:
+    return state;
   }
 }
